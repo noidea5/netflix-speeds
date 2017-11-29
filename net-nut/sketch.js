5a5d089af04f0d2f3c8898a42d418b86d1d8ab8b
noidea5 <32461629+noidea5@users.noreply.github.com>
2017-11-29 09:31:13 -0800

    Add files via upload

--- net-nut/sketch.js
+++ net-nut/sketch.js
@@ -28,9 +28,9 @@ function setup() {
 	radio.option("December 2013 ", 3);
 	radio.option("January 2014  ", 4);
 	radio.option("February 2014 ", 5);
-	radio.option("March 2014 ...", 6);
+	radio.option("March 2 0 1 4 ", 6);
 	radio.option("April 2014    ", 7);
-	radio.position(10, 300);
+	radio.position(10, 400);
 	radio.style("width", "140px");
 	radio.style("font-family", "arial");
 	textAlign(CENTER);
@@ -40,12 +40,14 @@ function setup() {
 	};
 };
 
+alert("For best results, please use Firefox");
+
 function draw() {
 	createCanvas(windowWidth, windowHeight);
 	background(100, 200, 255);
 
 	noStroke();
-	fill(50, 180, 240);
+	fill(204, 229, 255);
 	rect(0, windowHeight - 20, windowWidth, 23);//floor. Purely asthetic; the sprites don't collide directly.
 
 	fill(70);
@@ -57,72 +59,90 @@ function draw() {
 
 	val = radio.value();
 
+	//creates every block and gives them gravity
 	for (var i = 0; i < sprite.length; i++) {
 		sprite[i].fall();
 		sprite[i].display();
 	};
 
 	if (val) {
-		for (var i = 0; i < parseFloat(mbps.netflix[val].speed.toFixed(2)) * 100; i++) {
-			sprite[i].floor = "down";
-		};
-		for (var i = parseFloat(mbps.netflix[val].speed.toFixed(2)) * 100 + 1; i < sprite.length; i++) {
+		//once a radio has been selected, the appropriate blocks are made to fall up or down
+		for (var i = 0; i < parseFloat(mbps.netflix[val].speed.toFixed(2)); i++) {
 			sprite[i].floor = "up";
 		};
-	} else {
+		for (var i = parseFloat(mbps.netflix[val].speed.toFixed(2)); i < sprite.length; i++) {
+			sprite[i].floor = "down";
+		};
+		//text telling the month and speed
+		textSize(60);
+		fill(204, 229, 255);
+		noStroke();
+		text(`Netflix's speed was ${mbps.netflix[val].speed/100} mbps`, 200, 300);
+		text(`in ${mbps.netflix[val].date}.`, 200, 360);
+
 		textSize(30);
-		fill(230);
-		text("In October 2013, Netflix's speed on Comcast began slowing. Comcast was throttling its", 200,400);
-		text("consumers' access to Netflix to coerce the company into a network deal. In February", 200, 430);
-		text("2014, Netflix agreed to the deal, and its speed on Comcast skyrocketed, even exceeding",200,460);
-		text("its original speed in Septembr. It peaked in April 2014 at 2.77 mbps.", 200, 490);
+		text("Each block represents 10 kilobytes.", 200, 400);
+		text("Press any key to toss the blocks.", 200, windowHeight - 100);
+
+		fill(200);
+		text("^ Speed that month ^", 10, 100);
+	} else {//Introductory information for before a radio has been selected
+		textSize(30);
+		fill(204, 229, 255);
+		noStroke();
+		text("In October 2013, Netflix's speed on Comcast began slowing. Comcast was throttling its", 200, 300);
+		text("consumers' access to Netflix to coerce the company into a network deal. In February", 200, 330);
+		text("2014, Netflix agreed to the deal, and its speed on Comcast skyrocketed, even exceeding",200, 360);
+		text("its original speed in September. It peaked in April 2014 at 2.77 mbps.", 200, 390);
+
+		text("Press any key to toss the blocks. Select a month at left.", 200, 450);
+		text("The green blocks represent that month's speed.", 200,480);
 	};
 };
 
-//throws the sprites that have fallen
+//throws the blocks, mainly just for fun
 function keyPressed() {
 	for (var i = 0; i < 277; i++) {
 		sprite[i].tossed();
 	};
 };
 
-function displaySpeed() {
-	textSize(50);
-	fill(230);
-	text(`Netflix's speed was ${mbps.netflix[val].speed} mbps that month,`, 200, 300);
-	text(`${difference.toFixed(2)} mbps less than its peak in April.`, 200, 350);
-	textSize(25);
-	text("One block represents 100 kilobytes.", 200, 400);
-	text("Press any key to toss the blocks.", 200, windowHeight - 70);
-};
-
 function Sprite() {
 	this.x = random(0, windowWidth-40); //sprite's x position
 	this.y = random(5, 160); //y position
-	this.step = 20; //how fast they move left and right. Not currently being used.
-	this.xVelocity = 0; //used for left/right movement. Not in use.
-	this.intertia = 1; //how fast the sprite accelerates/deccelerates. Not in use.
 	this.yVelocity = 0; //used for falling.
-	this.gravity = 1; //Higher gravity = greater acceleration
-	this.direction = "none"; //used to denote which way the sprite faces. Not in use.
-	this.xToss = 20; //max speed sprite can be thrown in either direction. Not in use.
-	this.yToss = 30; //max height sprite can be thrown.
-	this.bounce = 0.8;
-	this.floor = "up";
+	this.gravity = random(0.65, 0.85);//Higher gravity = greater acceleration. Every block has a different
+	//gravity so that they don't fall in a line but they separate a bit
+	this.yToss = 20; //max height sprite can be thrown.
+	this.bounce = random(0.3, 0.5);//each block has a slightly different bounce factor
+	this.floor = "down";//"up" tells the blocks to fall up, vice versa with "down"
+	this.up = 0;//random(0, 50);
+	this.down = 63;//random(63, 95);//a variable to easily change the height of the ground
 
 	//draws the sprite
 	this.display = function() {
-		fill(255, 0, 150, 50);
+		if (this.floor === "down") {
+			fill(255, 0, 150, 50);
+		} else {
+			fill(150, 255, 0, 50);
+		};
+		// if (this.y === 0) {
+		// 	fill(150, 255, 0, 50);
+		// } else if (this.y === windowHeight - 63) {
+		// 	fill(255, 0, 150, 50);
+		// } else {
+		// 	noFill();
+		// };
 		stroke(255);
 		strokeWeight(5);
-		rect(this.x, this.y, 40, 40, 10);
+		rect(this.x, this.y, 40, 40, 50);
 	};
 
 	//invokes gravity. As long as the sprite is not on the "ground," it falls faster and faster.
 	this.fall = function() {
 		if (this.floor === "down") {
-			if (this.y + this.yVelocity >= windowHeight - 63) {
-				this.y = windowHeight - 63;
+			if (this.y + this.yVelocity >= windowHeight - this.down) {
+				this.y = windowHeight - this.down;
 				if (this.yVelocity > 1) {
 					this.yVelocity = -this.yVelocity * this.bounce;
 				} else {
@@ -133,8 +153,8 @@ function Sprite() {
 				this.y += this.yVelocity;
 			};
 		} else if (this.floor === "up") {
-			if (this.y + this.yVelocity <= 0) {
-				this.y = 0;
+			if (this.y + this.yVelocity <= this.up) {
+				this.y = this.up;
 				if (this.yVelocity < 1) {
 					this.yVelocity = -this.yVelocity * this.bounce;
 				} else {
@@ -159,64 +179,12 @@ function Sprite() {
 		};
 	};
 
-	//doesn't do anything right now. Needs to be re-written.
-	this.leftRight = function() {
-		if (this.y === windowHeight - 63) {
-			if (this.direction === "left") {
-				this.x += this.xVelocity * 0.5;
-
-				if (this.xVelocity < 0) {
-					this.xVelocity += this.intertia;
-				} else {
-					this.xVelocity = 0;
-				};
-
-			} else if (this.direction === "right") {
-				this.x += this.xVelocity * 0.5;
-
-				if (this.xVelocity > 0) {
-					this.xVelocity -= this.intertia;
-				} else {
-					this.xVelocity = 0;
-				};
-			};
-		};
-		this.x += this.xVelocity;
-	};
-
 	//gives every sprite a random x and y velocity to throw them around.
 	this.tossed = function() {
 		if (this.floor === "down") {
 			this.yVelocity = random(-12, -this.yToss);
 		} else if(this.floor === "up") {
 			this.yVelocity = random(12, this.yToss);
-		}
+		};
 	};
 };
-
-
-	// if (val) {
-	// 	for (let i = 0; i < parseFloat(mbps.netflix[val].speed.toFixed(2)) * 100; i++) {
-	// 		sprite[i] = new Sprite();
-	// 	};
-	// 	difference = 2.77 - mbps.netflix[val].speed;
-
-	// 	console.log(mbps.netflix[val].speed);
-	// 	displaySpeed();	
-
-	// 	for (let i = 0; i < sprite.length; i++) {
-	// 		sprite[i].display();
-	// 		sprite[i].fall();
-	// 	};
-	// 	loadFirst = 1;
-	// } else {
-	// };
-	// if (loadFirst === 0) {
-	// 	for (let i = 0; i < 277; i++) {
-	// 		sprite[i] = new Sprite();
-	// 	};
-	// 	for (let i = 0; i < sprite.length; i++) {
-	// 		sprite[i].display();
-	// 		sprite[i].fall();
-	// 	};
-	// };
